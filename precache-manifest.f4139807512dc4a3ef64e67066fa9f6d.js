f9913bc309eb2dfcc705282e8ccabff89ba90ba5
sadabdhrubo1997 <53494264+sadabdhrubo1997@users.noreply.github.com>
2020-09-20 15:49:10 +0600

    Updates

---
+++ precache-manifest.f4139807512dc4a3ef64e67066fa9f6d.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27fc27c89af588481360eddf2710f2dd",
+    "url": "/reactdeploy/index.html"
+  },
+  {
+    "revision": "3fad7ee30bf9d40b9561",
+    "url": "/reactdeploy/static/css/2.63c9f943.chunk.css"
+  },
+  {
+    "revision": "282890c4aaccb8797d25",
+    "url": "/reactdeploy/static/css/main.919095cf.chunk.css"
+  },
+  {
+    "revision": "3fad7ee30bf9d40b9561",
+    "url": "/reactdeploy/static/js/2.350315f8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/reactdeploy/static/js/2.350315f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "282890c4aaccb8797d25",
+    "url": "/reactdeploy/static/js/main.c15f037a.chunk.js"
+  },
+  {
+    "revision": "6f78dbc979adce080857",
+    "url": "/reactdeploy/static/js/runtime-main.d41547aa.js"
+  },
+  {
+    "revision": "a0be3812ea6b2f862512ce5a0c3fdbce",
+    "url": "/reactdeploy/static/media/AdobeArabicRegular.a0be3812.otf"
+  },
+  {
+    "revision": "4395b29d9210bd75051ebce1e66e186d",
+    "url": "/reactdeploy/static/media/AirbnbCerealBlack.4395b29d.ttf"
+  },
+  {
+    "revision": "c94c29feb80b02db176028d16fc634d2",
+    "url": "/reactdeploy/static/media/AirbnbCerealBold.c94c29fe.ttf"
+  },
+  {
+    "revision": "86b15ac031e0507ad790afafd9adf7f7",
+    "url": "/reactdeploy/static/media/AirbnbCerealBook.86b15ac0.ttf"
+  },
+  {
+    "revision": "0a0cbe1eca15f71b7187b38b1173eb98",
+    "url": "/reactdeploy/static/media/AirbnbCerealExtraBold.0a0cbe1e.ttf"
+  },
+  {
+    "revision": "1f9c7d320137974038c2f1a7300a1577",
+    "url": "/reactdeploy/static/media/AirbnbCerealLight.1f9c7d32.ttf"
+  },
+  {
+    "revision": "d9fb3054e00c65841121da81c48e4fb1",
+    "url": "/reactdeploy/static/media/AirbnbCerealMedium.d9fb3054.ttf"
+  },
+  {
+    "revision": "ebf2f3f4fa6920c7ea512bd4c2cca145",
+    "url": "/reactdeploy/static/media/close.ebf2f3f4.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/reactdeploy/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/reactdeploy/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/reactdeploy/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/reactdeploy/static/media/slick.f97e3bbf.svg"
+  }
+]);
