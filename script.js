f0850f5aa51892ffd990a43fe6e0776d1a699cc8
sadabdhrubo1997 <53494264+sadabdhrubo1997@users.noreply.github.com>
2020-11-23 04:11:32 +0600

    Updates

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 window.addEventListener("DOMContentLoaded", function () {
+
+       // Responsive html font size
+       const iw = window.innerWidth;
+       const rootScreenWidth = 1366;
+       const rootFontPercentage = 44.46;  
+       
+       
+       const my_html = document.querySelector("html");
+       my_html.style.fontSize=rootFontPercentage+"%"
+      
+   
+      
+       if (iw <= 1366) {
+         my_html.style.fontSize=`${rootFontPercentage}%`;        
+       }else{
+           let currentFontSize = (rootFontPercentage * iw) / rootScreenWidth;
+           my_html.style.fontSize=`${currentFontSize}%`;    
+       }
+         
+
+
+
+
     window.addEventListener('resize', () => {
-     // Responsive html font size
-    const iw = window.innerWidth;
-    const rootScreenWidth = 1366;
-    const rootFontPercentage = 44.46;    
+ 
     
     const my_html = document.querySelector("html");
     my_html.style.fontSize=rootFontPercentage+"%"
